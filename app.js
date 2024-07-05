266d8d1e5c4d8fe600a59dd71246a1929a9c5c27
amrihamdi <hamdiamri53@GMAIL.COM>
2024-07-05 20:29:36 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const categorieRouter =require("./routes/categorie.route")
+const scategorieRouter=require("./routes/scategorie.route")
+const articleRouter =require("./routes/article.route")
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+const mongoose = require('mongoose');
+app.use(express.json());
+app.get("/",(req, res) => {
+res.send("bienvenue sur notre sites")
+})
+mongoose.connect(process.env.DATABASECLOUD, {useNewUrlParser: true, useUnifiedTopology: true})
+.then(() => {console.log("DataBase Successfully Connected");})
+.catch(err => { console.log("Unable to connect to database", err);
+process.exit(); });
+app.use('/api/categories', categorieRouter);
+app.use('/api/scategories',scategorieRouter);
+app.use('/api/articles', articleRouter);
+app.listen(process.env.PORT)
+console.log("application run port  "+process.env.PORT  )
---
+++ routes/categorie.route.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const router = express.Router();
+// Créer une instance de categorie.
+const Categorie = require('../models/categorie');
+// afficher la liste des categories.
+router.get('/', async (req, res, )=> {
+    try {
+    const getcategorie = await Categorie.find({}, null ,{sort: {_id: -1}});
+    res.status(200).json(getcategorie);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    
+});
+// créer un nouvelle catégorie
+
+router.post('/', async (req, res) => {
+    const { nomcategorie, imagecategorie} = req.body;
+    const postCategorie = new Categorie({nomcategorie:nomcategorie,
+    imagecategorie:imagecategorie})
+    try {
+    await postCategorie.save();
+    res.status(200).json(postCategorie);
+    } catch (error) {
+    res.status(404).json({ message: error.message });
+    }
+    });
+// chercher une catégorie
+router.get('/:categorieId',async(req, res)=>{
+    try {
+    const findCategorie = await Categorie.findById(req.params.categorieId);
+   
+    res.status(200).json(findCategorie);
+    } catch (error) {
+    res.status(400).json({ message: error.message });
+    }
+    
+ 
+});
+// modifier une catégorie
+router.put('/:categorieId', async (req, res)=> {
+    try {
+        const putcategorie = await Categorie.findByIdAndUpdate(req.params.categorieId,
+        { $set: req.body },
+        { new: true }
+        );
+        res.status(200).json(putcategorie);
+        } catch (error) {
+        res.status(404).json({ message: error.message });
+        }
+});
+// Supprimer une catégorie
+router.delete('/:categorieId', async (req, res)=> {
+    try {
+     await Categorie.findByIdAndDelete(req.params.categorieId);
+    
+
+    res.status(200).json({message: 'Catégorie supprimée'});
+    } catch (error) {
+    res.status(400).json({ message: error.message });
+    }
+    
+});
+module.exports = router;
+// créer une nouvelle catégorie
