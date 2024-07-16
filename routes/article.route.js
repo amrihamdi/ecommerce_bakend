726546f4ead902e272b7342f91af96254ad95ac9
amrihamdi <hamdiamri53@GMAIL.COM>
2024-07-16 20:42:12 +0100

    version5

--- routes/article.route.js
+++ routes/article.route.js
@@ -57,12 +57,13 @@ router.put('/:articleId', async (req, res)=> {
     }
     });
     router.get('/art/pagination', async(req, res) => {
+        const filtre = req.query.filtre || "";
         const page = parseInt(req.query.page);
         const pageSize = parseInt(req.query.pageSize);
         // Calculate the start and end indexes for the requested page
         const startIndex = (page - 1) * pageSize;
         const endIndex = page * pageSize;
-        const articles = await Article.find().populate("scategorieID").exec()
+        const articles = await Article.find({}).populate("scategorieID").exec()
         // Slice the products array based on the indexes
         const paginatedProducts = articles.slice(startIndex, endIndex);
         // Calculate the total number of pages
@@ -70,5 +71,6 @@ router.put('/:articleId', async (req, res)=> {
         // Send the paginated products and total pages as the API response
         res.json({ products: paginatedProducts, totalPages });
         });
+        const articles=await article.find({designation:{$regex:filter,$options:"i"}},null,{sort:{'_id':-1}}).populate("scategorieID").exec
         
     module.exports = router;
